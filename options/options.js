1669354d2bd4b8b9edcbfea828e46e3cf5af6b2f
Crowdbotics <team@crowdbotics.com>
2023-09-19 14:36:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jerrick-dickson-app-43456",
+  url: "https://jerrick-dickson-app-43456.botics.co",
+  api: "https://jerrick-dickson-app-43456.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
